27be264ca89be9bf35f9f31d342a82a14d1e3c79
Kumar Varadarajulu <kumarvaradarajulu@gmail.com>
2015-02-15 00:33:20 +0530

    Visual changes to post page

--- blog/forms.py
+++ blog/forms.py
@@ -15,28 +15,29 @@ class BlogEntryForm(forms.Form):
     image = ImageFormField()
     tags = TagField()
     content = forms.CharField(widget=CKEditorWidget())
-    score = forms.DecimalField(max_value=settings.RATING_SCALE, max_digits=settings.RATING_MAX_DIGITS)
+    score = forms.DecimalField(max_value=settings.RATING_SCALE, max_digits=settings.RATING_MAX_DIGITS, min_value=0)
 
     def __init__(self, *args, **kwargs):
         super(BlogEntryForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.helper.form_class = 'pure-form pure-form-stacked pure-u-1'
         self.helper.layout = Layout(
             Div(
-                Button('reset', 'Reset Section', data_section_id=1, data_btn_nm='reset', wrapper_class='rep', css_class='sec_btn'),
-                Button('del', 'Delete Section', data_section_id=1, data_btn_nm='del', wrapper_class='rep', css_class='sec_btn'),
-                Field('title', wrapper_class='rep', css_class='rep'),  # Class rep tells it is replicable item
-                Field('score', wrapper_class='rep', css_class='rep'),  # Class rep tells it is replicable item
-                Field('image', wrapper_class='norep', css_class='rep'),  # Class norep tells the item must be removed from replica's
-                Field('content', wrapper_class='rep', css_class='rep'),
+                Button('reset', 'Reset Section', data_section_id=1, data_btn_nm='reset', wrapper_class='rep', css_class='sec_btn pure-button'),
+                Button('del', 'Delete Section', data_section_id=1, data_btn_nm='del', wrapper_class='rep', css_class='sec_btn pure-button'),
+                Field('title', wrapper_class='rep pure-control-group', css_class='rep'),  # Class rep tells it is replicable item
+                Field('score', wrapper_class='rep pure-control-group', css_class='rep'),  # Class rep tells it is replicable item
+                Field('image', wrapper_class='norep pure-control-group', css_class='rep'),  # Class norep tells the item must be removed from replica's
+                Field('content', wrapper_class='rep pure-control-group', css_class='rep'),
                 css_class='section',
                 css_id='sec',
                 data_section_id=1
             ),
-            Field('tags'),
+            Field('tags', wrapper_class='norep pure-control-group', css_class='norep'),
             ButtonHolder(
-                Button('add', 'Add Section', data_btn_nm='add', data_section_count=1, css_id='add', css_class='sec_btn'),
-                Button('save', 'Save Section', css_id='save'),
-                Submit('submit', 'Submit Post', css_id='submit'),
+                Button('add', 'Add Section', data_btn_nm='add', data_section_count=1, css_id='add', css_class='sec_btn pure-button'),
+                Button('save', 'Save Section', css_id='save', css_class='pure-button'),
+                Submit('submit', 'Submit Post', css_id='submit', css_class='pure-button pure-button-primary'),
                 css_id='buttons'
             ),
         )
