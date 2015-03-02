1f2d22b13e7907f79def851d4e15a9713a15a502
Kumar Varadarajulu <kumarvaradarajulu@gmail.com>
2015-03-02 23:21:08 +0530

    List page with images

--- django_andblog/settings.py
+++ django_andblog/settings.py
@@ -69,6 +69,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'social.apps.django_app.context_processors.backends',
     'social.apps.django_app.context_processors.login_redirect',
     'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.media',
 )
 
 ROOT_URLCONF = 'django_andblog.urls'
