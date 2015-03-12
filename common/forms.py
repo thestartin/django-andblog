13aca6e7d2b910bf9c038b1c71952dd0cf171bf1
Kumar Varadarajulu <kumarvaradarajulu@gmail.com>
2015-03-12 11:03:34 +0530

    Login/Register Form changes

--- common/forms.py
+++ common/forms.py
@@ -1,39 +1,63 @@
+import re
+
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Field, Submit, Div
+from crispy_forms.layout import Layout, Field, Submit, Div, Hidden
+
+
+EMAIL_REGEX = re.compile(r".*?@.*?\..*?")
+
+
+def user_name_email(value):
+    if EMAIL_REGEX.match(value):
+        # User has supplied an email validate it
+        validate_email(value)
+    else:
+        if len(value) > 30:
+            raise ValidationError('User Name must be less than 15 chars')
 
 
 class LoginForm(forms.Form):
-    user_name_email = forms.CharField(max_length=255)
-    password = forms.CharField(widget=forms.PasswordInput())
+    log_user_name_email = forms.CharField(max_length=255, label='User Name or Email', validators=[user_name_email, ])
+    log_password = forms.CharField(widget=forms.PasswordInput())
 
     def __init__(self, *args, **kwargs):
-        super(LoginForm, self).__init__(self, *args, **kwargs)
+        super(LoginForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
             Div(
-                Field('user_name_email'),
-                Field('password'),
+                Field('log_user_name_email', placeholder='Email or Username'),
+                Field('log_password', placeholder='Password'),
+                Hidden('formname', value='loginform'),
                 Submit('login', 'Login'),
             )
         )
 
+    def __str__(self):
+        return 'loginform'
+
 
 class RegisterForm(forms.Form):
-    user_name = forms.CharField(max_length=15)
-    user_email = forms.CharField(max_length=255)
-    password = forms.CharField(widget=forms.PasswordInput())
-    confpassword = forms.CharField(widget=forms.PasswordInput())
+    reg_user_name = forms.CharField(max_length=30)
+    reg_user_email = forms.EmailField(label='Email')
+    reg_password = forms.CharField(widget=forms.PasswordInput())
+    reg_confpassword = forms.CharField(widget=forms.PasswordInput(), label='Confirm Password')
 
     def __init__(self, *args, **kwargs):
-        super(RegisterForm, self).__init__(self, *args, **kwargs)
+        super(RegisterForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
             Div(
-                Field('user_name'),
-                Field('user_email'),
-                Field('password'),
-                Field('confpassword'),
+                Field('reg_user_name', placeholder='User Name'),
+                Field('reg_user_email', placeholder='Email'),
+                Field('reg_password', placeholder='Password'),
+                Field('reg_confpassword', placeholder='Confirm Password'),
+                Hidden('formname', value='registerform'),
                 Submit(name='register', value='Register'),
             )
         )
+
+    def __str__(self):
+        return 'registerform'
--- common/views.py
+++ common/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from django.views.generic import FormView
+from django.http import HttpResponseBadRequest
 
 from .forms import LoginForm, RegisterForm
 from .mixins import MultiFormMixin
@@ -13,3 +13,47 @@ class LoginRegisterView(MultiFormMixin, FormView):
         'registerform': RegisterForm
     }
 
+    success_url = '/login'
+
+    def post(self, request, *args, **kwargs):
+        formname = request.POST.get('formname')
+        if not formname:
+            return HttpResponseBadRequest()
+
+        form_class = self.get_form_class(form_name=formname)
+        form = self.get_form(form_class, formname)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_invalid(self, form):
+        """
+        If the form is invalid, re-render the context data with the
+        data-filled form and errors.
+        """
+        return self.render_to_response(self.get_context_data(form=self.get_all_other_forms(form)))
+
+    def get_all_other_forms(self, form):
+        forms = [form]
+        for form_name, form_class in self.form_classes.iteritems():
+            if form_name != str(form):
+                forms.append(form_class(**self.get_form_kwargs(others=True)))
+
+        return forms
+
+    def get_form_kwargs(self, others=False):
+        """
+        Returns the keyword arguments for instantiating the form.
+        """
+        kwargs = {
+            'initial': self.get_initial(),
+            'prefix': self.get_prefix(),
+        }
+
+        if self.request.method in ('POST', 'PUT') and not others:
+            kwargs.update({
+                'data': self.request.POST,
+                'files': self.request.FILES,
+            })
+        return kwargs
