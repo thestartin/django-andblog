3beccafb55342e25d777d5cbe6257a6e92614178
Kumar Varadarajulu <kumarvaradarajulu@gmail.com>
2015-03-13 11:02:56 +0530

    Login/register half commit

--- common/mixins.py
+++ common/mixins.py
@@ -19,7 +19,7 @@ class MultiFormMixin(object):
 
         return tuple(self.form_classes.itervalues())
 
-    def get_form(self, form_classes, form_name=None):
+    def get_form(self, form_classes, exclude=None):
         if isinstance(form_classes, tuple):
             forms = []
             for form_class in form_classes:
@@ -28,3 +28,34 @@ class MultiFormMixin(object):
             return forms
         else:
             return form_classes(**self.get_form_kwargs())
+
+    def get_all_other_forms(self, form):
+        forms = [form]
+        for form_name, form_class in self.form_classes.iteritems():
+            if form_name != str(form):
+                forms.append(form_class(**self.get_form_kwargs(others=True)))
+
+        return forms
+
+    def get_form_kwargs(self, others=False):
+        """
+        Returns the keyword arguments for instantiating the form.
+        """
+        kwargs = {
+            'initial': self.get_initial(),
+            'prefix': self.get_prefix(),
+        }
+
+        if self.request.method in ('POST', 'PUT') and not others:
+            kwargs.update({
+                'data': self.request.POST,
+                'files': self.request.FILES,
+            })
+        return kwargs
+
+    def form_invalid(self, form):
+        """
+        If the form is invalid, re-render the context data with the
+        data-filled form and errors.
+        """
+        return self.render_to_response(self.get_context_data(form=self.get_all_other_forms(form)))
--- common/views.py
+++ common/views.py
@@ -27,33 +27,6 @@ class LoginRegisterView(MultiFormMixin, FormView):
         else:
             return self.form_invalid(form)
 
-    def form_invalid(self, form):
-        """
-        If the form is invalid, re-render the context data with the
-        data-filled form and errors.
-        """
-        return self.render_to_response(self.get_context_data(form=self.get_all_other_forms(form)))
 
-    def get_all_other_forms(self, form):
-        forms = [form]
-        for form_name, form_class in self.form_classes.iteritems():
-            if form_name != str(form):
-                forms.append(form_class(**self.get_form_kwargs(others=True)))
-
-        return forms
-
-    def get_form_kwargs(self, others=False):
-        """
-        Returns the keyword arguments for instantiating the form.
-        """
-        kwargs = {
-            'initial': self.get_initial(),
-            'prefix': self.get_prefix(),
-        }
-
-        if self.request.method in ('POST', 'PUT') and not others:
-            kwargs.update({
-                'data': self.request.POST,
-                'files': self.request.FILES,
-            })
-        return kwargs
+    def form_valid(self, form):
+        self.request.user.login
