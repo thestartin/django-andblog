6f5d686f410b8fee0825dc7bb54582e5704a482d
Kumar Varadarajulu <kumarvaradarajulu@gmail.com>
2015-05-10 22:00:21 +0530

    Gzip files and git post-commit hook

--- django_andblog/scripts/compress_css_js.sh
+++ django_andblog/scripts/compress_css_js.sh
@@ -5,17 +5,32 @@ CSS_DIR="$DIR/../assets/css"
 JS_DIR="$DIR/../assets/js"
 
 cd $CSS_DIR
-for i in main.css pure.css
+filename="min.all.css.gz"
+[ -e $filename ] && rm "$filename"
+filename="min.all.css"
+[ -f $filename ] && rm "$filename"
+
+for i in pure.css main.css
 do
   echo "Creating minified CSS for $i"
   java -jar ~/yuicompressor/yuicompressor-2.4.8.jar --type css -o "min.$i" "$i"
+  cat "min.$i" >> min.all.css
 done
+gzip min.all.css
 
 cd $JS_DIR
+filename="min.all.js.gz"
+[[ -e $filename ]] && rm "$filename"
+filename="min.all.js"
+[[ -f $filename ]] && rm "$filename"
+
 for i in main.js plugins.js
 do
   echo "Creating minified JS for $i"
   java -jar ~/yuicompressor/yuicompressor-2.4.8.jar --type js -o "min.$i" "$i"
+  cat "min.$i" >> min.all.js
 done
+
+gzip min.all.js
 
 echo "Done!"
