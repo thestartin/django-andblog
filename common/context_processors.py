209bdc7d9ce581cc829ce13c310bde636bccd142
Kumar Varadarajulu <kumarvaradarajulu@gmail.com>
2015-05-12 22:41:35 +0530

    Various UI fixes, fully responsive

--- common/context_processors.py
+++ common/context_processors.py
@@ -7,5 +7,6 @@ def meta_data(request):
     meta['site_url'] = settings.SITE_URL
     meta['site_db_admins'] = settings.SITE_FB_ADMINS
     meta['site_bitly_verification'] = settings.SITE_BITLY_VERIFICATION
+    meta['DEBUG'] = settings.DEBUG
     meta['accept_gzip'] = 'gzip' in request.META.get('HTTP_ACCEPT_ENCODING', '')
     return {'meta': meta}
